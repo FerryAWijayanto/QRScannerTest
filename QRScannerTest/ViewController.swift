03b07020e2877495cd9f50da086117119a8f653d
Ferry <ferry.wijayanto@aladinbank.id>
2023-11-11 13:55:49 +0700

    Initial Commit

---
+++ QRScannerTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRScannerTest
+//
+//  Created by Ferry Adi Wijayanto on 11/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
